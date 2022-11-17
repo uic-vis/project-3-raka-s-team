0d7b311eeef11eec666dc2a055ea9b750f96fc19
Raka Primardika <rakadwinanto@gmail.com>
2022-11-17 10:42:24 -0600

    lab 17/11

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function getMap(coordinates, zoom) {
 
   // color = d3.scaleLinear()
   //   .domain(d3.extent(statesData, d => screen.properties.density)).nice()
-  //   .range(d3.interpolateTurbo)
+  //   .range([0,1]);
 
   function getColor(d) {
     return d > 1000 ? '#91003f' :
@@ -22,17 +22,28 @@ function getMap(coordinates, zoom) {
   }
 
   function style(feature) {
+
+    var colorScale = d3.scaleQuantize()
+      .domain([0, 1000])
+      .range(colorbrewer.YlOrRd[9]);
+
     return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
+      // fillColor: getColor(feature.properties.density),
+      // fillColor: "#ff00ff",
+      fillColor: colorScale(feature.properties.density),
+      weight: 2,
+      opacity: 1,
+      color: 'black',
+      dashArray: '1',
+      fillOpacity: 0.7
     };
   }
 
-  L.geoJSON(statesData, {style: style}).addTo(map);
+  function onEachFeature(feature, layer) {
+    console.log(feature);
+  }
+
+  L.geoJSON(statesData, {style: style, onEachFeature: onEachFeature}).addTo(map);
 }
 
 d3 = require("d3")
