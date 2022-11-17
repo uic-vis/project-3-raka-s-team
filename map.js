3d58a106b05ccfd996dd0582647a66db241fb014
Raka Primardika <rakadwinanto@gmail.com>
2022-11-17 08:54:39 -0600

    Added Leaflet Map

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([41.8781, -87.6498], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
